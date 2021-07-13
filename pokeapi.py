1e8702f81182e2ac8c547d5fb66885091ed0b0ad
Yuya Ra <68813586+YuyaR@users.noreply.github.com>
2021-07-14 08:23:44 +0900

    a second option to calling pokemons
    also added call pokemon by name method.

--- pokeapi.py
+++ pokeapi.py
@@ -1,39 +1,32 @@
 import requests
-# import pprint
 
 def get_pokemon(n):
+    pokestat = {}
     ROOT = 'https://pokeapi.co/api/v2/'
-    pokemon_ls = []
-    for i in range(1, n + 1):
-        pokemon = requests.get(ROOT + f'pokemon/{i}')
-        pokemon_ls.append(pokemon)
-    
-    return pokemon_ls
-    
 
-def get_attack(json):
-# Second in the list
-    attack = json['stats'][1]['base_stat']
-    return attack
+    for i in range(1, n+1):
+        link = f'pokemon/{i}'
+        r = requests.get(ROOT+link)
+        pname = r.json()['name']
+        pokestat[pname] = {'hp':None, 'attack':[], 'defense':[], 'elemental_type':[]}
 
-def get_hp(json):
-# First in the list
-    attack = json['stats'][0]['base_stat']
-    return attack
+        for n in range(0,3):
+            stat = r.json()['stats'][n]['base_stat']
+            stat_type = r.json()['stats'][n]['stat']['name']
+            pokestat[pname][stat_type] = stat
+        pokestat[pname]['elemental_type'] = r.json()['types'][0]['type']['name']
+    return pokestat
 
-def get_defense(json):
-# Third in the list
-    attack = json['stats'][2]['base_stat']
-    return attack
+def get_pokemon_byName(x):
+    pokename = x.lower()
+    r = requests.get('https://pokeapi.co/api/v2/pokemon/' + pokename)
+    pokestat = {}
+    pokestat[pokename]= {'hp':None, 'attack':[], 'defense':[], 'elemental_type':[]}
 
+    for n in range(0,3):
+        stat = r.json()['stats'][n]['base_stat']
+        stat_type = r.json()['stats'][n]['stat']['name']
+        pokestat[pokename][stat_type] = stat
+    pokestat[pokename]['elemental_type'] = r.json()['types'][0]['type']['name']
 
-if __name__ == '__main__':
-    pokemon_ls = get_pokemon(1)
-    # print(pokemon_ls[0].json())
-    # print(pokemon_ls[0].json().keys())
-    # print(pokemon_ls[0].json()['name'])
-    # print(pokemon_ls[0].json()['types'][0]['type']['name'])
-    for item in pokemon_ls[0].json()['stats']:
-        print(item)
-    # print(pokemon_ls[0].json()['stats'])
-    # print(pokemon_ls[0].json()['stats'][0]['stat']['name'])
+    return pokestat
