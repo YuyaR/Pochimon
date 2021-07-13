c90ecbd020fc8697013d713f0633f9d4cb58c393
Yuya Ra <68813586+YuyaR@users.noreply.github.com>
2021-07-14 08:35:45 +0900

    second option for initialising pokemon

--- main.py
+++ main.py
@@ -1,49 +1,42 @@
-#%%
 from Game import *
-import pokeapi
-import os
 
-pokemon_ls = pokeapi.get_pokemon(12)
-#%%
-# pokemon_dict = {}
-# print(pokemon_ls[0].json()['name'])
-pok_list = []
-for pokemon in pokemon_ls:
-    attack = pokeapi.get_attack(pokemon.json())
-    defense = pokeapi.get_defense(pokemon.json())
-    hp = pokeapi.get_hp(pokemon.json())
-    name = pokemon.json()['name']
-    new_pok = Pokemon.Pokemon(hp, name=name.upper(), attack=attack, defense=defense, elemental_type='Fire')
-    pok_list.append(new_pok)
-    # name = pokemon
-    # print(f'Attack = {attack}')
-    # print(defense)
-    # print(hp)
-print(pok_list)
-#%%
-# pound = Pokemon.Attack('Pound', 40, 30, 'Normal')
-# ember = Pokemon.Attack('Ember', 40, 20, 'Fire')
-# vine_whip = Pokemon.Attack('Vine Whip', 45, 25, 'Grass')
-# charmander_moveset = [pound, ember]
-# bulbasaur_moveset = [pound, vine_whip]
 
-# charmander = Pokemon.Pokemon(200, 'Charmander', 30, 20, 'Fire', charmander_moveset)
-# bulbasaur = Pokemon.Pokemon(200, 'Bulbasaur', 30, 20, 'Grass', bulbasaur_moveset)
+#use pokestat to instantiate
+pokedex = []
+
+
+pokelist = pokeapi.get_pokemon(11)
+pokelist.update(pokeapi.get_pokemon_byName('pikachu'))
+
+
+for po in list(pokelist.keys()):
+    pokedex.append(pokemon.Pokemon(po.capitalize(), pokelist[po]['hp'], pokelist[po]['attack'], pokelist[po]['defense'], 'Normal'))
+
+# for obj in pokedex:
+#     print(obj.name)
+
+# satoshi = trainer.Trainer(pokedex[6:])
+# shigeru = trainer.Trainer(pokedex[1:6])
+# print(satoshi.pokemon)
+# print(shigeru.pokemon)
+
+# Pound = pokemon.Attack('Pound', 40, 30, 30, 'normal')
+# FireBlast = pokemon.Attack('Fire Blast', 100, 10, 10, 'Fire')
+# Thunder = pokemon.Attack('Thunder', 120, 5, 5, 'fire')
+# Jump = pokemon.Attack('Jump', 0, 40, 40, 'normal')
+
+# pikachu = pokemon.Pokemon('Pikachu', 200, 30, 20, 'Normal', [Pound, FireBlast, Thunder])
+# bulbasaur = pokemon.Pokemon('Bulbasaur', 200, 30, 20, 'Grass')
+# magikarp = pokemon.Pokemon('Magikarp', 150, 10, 10, 'Water', Jump)
 
 # while True:
-#     os.system('cls' if os.name == 'nt' else 'clear')
-#     print(f'What will {charmander.name} do?')
-#     flag = charmander.fight(bulbasaur)
-#     if flag == False:
+#     if pikachu.hp>0:
+#         pikachu.fight(bulbasaur)
+#     else:
+#         print("Pikachu is incapacitated!")
+#     if bulbasaur.hp>0:
+#         bulbasaur.fight(pikachu)
+#     else:
+#         print("Bulbasaur is incapacitated!")
 #         break
-#     Pokemon.slow_print(f'{bulbasaur.name} has {bulbasaur.hp} points left\n')
-#     input('Press Enter to continue')
-#     os.system('cls' if os.name == 'nt' else 'clear')
-#     print(f'What will {bulbasaur.name} do?')
-#     flag = bulbasaur.fight(charmander)
-#     if flag == False:
-#         break
-#     Pokemon.slow_print(f'{charmander.name} has {charmander.hp} points left\n')
-#     input('Press Enter to continue')
 
-# %%
